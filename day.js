e63c1d1d13f968ed3232e5f8e6985971264efb3c
Mohamad <mm.nemrawi96@gmail.com>
2020-04-15 16:26:48 +0300

    saved

--- day.js
+++ day.js
@@ -1,17 +1,37 @@
 'use strict'
+
+
 var Q1 = prompt('What is the product of summing 1 + 1 to write your answer?');
 var Q2 = prompt('What is your favorite movies ?');
 var Q3 = prompt('Type your triple name?');
- 
-var ans1 ;
+var Q4 = prompt('how old are you ?');
 
-if (Q1 == 2){
+
+function  yourAge(Q4)
+{
+    Q4 = parseInt(Q4);
+   
+    var yourYear = 2020 - Q4 ;
+
+    return document.write('<p id="Q4">' + yourYear + '</p>');
+}
+console.log (Q4);
+
+yourAge(Q4);
+
+
+
+
+var ans1;
+
+
+if (Q1 == 2) {
     ans1 = 'you are smart good';
 }
 else {
     ans1 = 'wrong try again';
 }
 
-document.write('<p id="Q2">'+ans1+'</p>');
-document.write('<p id="Q2">'+Q2+'</p>');
-document.write('<p id="Q3">'+Q3+'</p>');
+document.write('<p id="Q2">' + ans1 + '</p>');
+document.write('<p id="Q2">' + Q2 + '</p>');
+document.write('<p id="Q3">' + Q3 + '</p>');
